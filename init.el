53304bf022d192fc10ef78afb4709a435bf74c92
jclmntn <jclmntn@gmail.com>
2025-04-02 06:58:21 -0300

    feat: friendship ended with org-roam now denote is my best friend

--- init.el
+++ init.el
@@ -14,11 +14,14 @@
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
+(setq-default default-process-coding-system '(utf-8 . cp1252))
 ;(setq coding-system-for-read 'utf-8)
 ;(setq coding-system-for-write 'utf-8)
 ;(setq keyboard-coding-system 'utf-8-unix)
 ;(setq terminal-coding-system 'utf-8-unix)
 ;(setq buffer-file-coding-system 'utf-8-unix) 
+
+;; (setq exec-path (append exec-path '("C:/msys64/mingw64/bin")))
 
 ;; Configurações de início
 (setq inhibit-startup-message t ; Tira a mensagem inicial
@@ -108,8 +111,9 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" default))
+ '(doc-view-resolution 400)
  '(package-selected-packages
-   '(org-super-agenda company python-ts-mode flycheck-eglot python-mode citre catpuccin-theme org-roam visual-fill-column org-bullets evil-magit magit counsel-projectile projectile undo-tree evil-collection evil general which-key rainbow-delimiters counsel compat doom-modeline ivy)))
+   '(citar-denote denote-journal consult-notes denote pet citar-org-roam citar pdf-tools org-noter jinx ef-themes solarized-theme yasnippet with-editor queue dash s ts transient swiper spinner f shrink-path sesman parseclj parseedn ht emacsql magit-section nerd-icons git-commit ivy-rich goto-chg flycheck annalist doom-themes clojure-mode catppuccin-theme all-the-icons xterm-color cider org-super-agenda company python-ts-mode flycheck-eglot python-mode citre catpuccin-theme org-roam visual-fill-column org-bullets evil-magit magit counsel-projectile projectile undo-tree evil-collection evil general which-key rainbow-delimiters counsel compat doom-modeline ivy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -132,7 +136,6 @@
              :config
              (setq ivy-initial-inputs-alist nil)) ; Não inicia buscas com ^
 
-
 ;; Ícones par ao modeline
 ;; Rodar: M-x all-the-icons-install-fonts para instalar as fontes
 (use-package all-the-icons)
@@ -150,12 +153,18 @@
 ;;                           doom-themes-enable-italic t)
 ;;             (load-theme 'doom-palenight t)
 ;;             )
-(use-package catppuccin-theme
-	    :ensure t
-	    :config (setq catppuccin-flavor 'mocha)
-	    (load-theme 'catppuccin :no-confirm)
-	    )
+;; (use-package catppuccin-theme
+;; 	    :ensure t
+;; 	    :config (setq catppuccin-flavor 'latte)
+;; 	    (load-theme 'catppuccin :no-confirm)
+;; 	    )
 
+(use-package ef-themes
+  :config (load-theme 'ef-elea-dark :no-confirm))
+
+;; (use-package solarized-theme
+;;   :ensure t
+;;   :config (load-theme 'solarized-light t))
 
 ;; Facilita edição de elisp
 (use-package rainbow-delimiters
@@ -183,22 +192,29 @@
 ;  "C-e" 'counsel-switch-buffer)
 
 (jclmntn/leader-keys
-  "o"  '(:ignore t :which-key "org-mode")
+  "o"	'(:ignore t :which-key "org-mode")
   "oa"  '(org-agenda :which-key "org-agenda")
   "oc"  '(org-capture :which-key "org-capture")
-  "or"  '(:ignore t :which-key "org-roam")
-  "orl"  '(org-roam-buffer-toggle :which-key "toggle buffer")
-  "orf"  '(org-roam-node-find :which-key "find node")
-  "ori"  '(org-roam-node-insert :which-key "insert node")
-  "orc"  '(org-roam-capture :which-key "capture")
-  "orj"  '(org-roam-dailies-capture-today :which-key "capture daily")
-  ":"  '(counsel-M-x :which-key "M-x") 
-  "."  '(projectile-find-file :which-key "find file") 
-  "/"  '(projectile-ripgrep :which-key "live grep projects") 
+  ":"	'(counsel-M-x :which-key "M-x") 
+  "."	'(projectile-find-file :which-key "find file") 
+  "/"	'(projectile-ripgrep :which-key "live grep projects")
+  "RET" '(counsel-bookmark :which-key "bookmarks")
+  ;; Denote keybindings
+  "n"   '(:ignore t :which-key "denote")
+  "nn"  '(denote :which-key "create denote note")
+  "nr"  '(denote-rename-file :which-key "rename file with denote")
+  "nl"  '(denote-link :which-key "link to denote file")
+  "nf"  '(consult-notes :which-key "find note")
+  "nj"  '(denote-journal-new-or-existing-entry :which-key "create or open journal entry")
+  "nk"  '(denote-journal-link-or-create-entry :which-key "link to journal entry")
+  "nb"  '(denote-backlinks :which-key "find denote backlinks")
+  "nw"  '(:ignore t :which-key "citar-denote")
+  "nwc" '(citar-create-note :which-key "create bibliographic note")
+  "nwx" '(citar-denote-nocite :which-key "find notes without citations")
   )
 
 ;; Vim Keybindings!!
-; Não me vejo querendo que algum buffer comece sem vim keybindings, mas se um dia acontecer...
+; Não me vejo querendo que algum modo comece sem vim keybindings, mas se um dia acontecer...
 ; (defun jclmntn/evil-hook ()
 ;  (dolist (mode '(custom-mode
 ;                  eshell-mode
@@ -243,6 +259,7 @@
   :diminish
   :config
   (evil-set-undo-system 'undo-tree)
+  (setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo/")))
   (global-undo-tree-mode 1))
 
 ;; Live fucking grep!!
@@ -258,7 +275,7 @@
   (setq projectile-switch-project-action #'projectile-dired))
 
 (use-package counsel-projectile
-  :config (counsel-projectile-mode)) ; M-o permite utilizar opções adicionais, inclusive usar o rg em um diretório inteiro.
+  :config (counsel-projectile-mode)) 
 
 ;; Fucking magit!!
 (use-package magit
@@ -308,20 +325,19 @@
   :hook (org-mode . jclmntn/org-mode-setup)
   :config
   (setq org-hide-emphasis-markers t)
-
+  (setq org-cite-global-bibliography '("~/Repos/Notes/bibs/lista_de_leitura.bib" "~/Repos/Notes/bibs/mba_literatura.bib"))
+  (setq org-agenda-window-setup 'only-window)
+  (setq org-agenda-restore-windows-after-quit t)
   (setq org-agenda-start-with-log-mode t)
   (setq org-log-done 'time)
   (setq org-log-into-drawer t)
 
   (setq org-agenda-files '("~/Repos/Notes/Tasks.org"))
-  (setq org-cite-global-bibliography '("~/Repos/Notes/lista_de_leitura.bib"))
 
   (setq org-todo-keywords
 	'((sequence "TODO(t!)" "NEXT(n)" "IDEA(i)" "|" "DONE(d!)" "KILL(k!)")))
 
-  (setq org-refile-targets '((("Archive.org" :maxlevel . 1)
-			      ("Tasks.org" :maxlevel . 1)
-			     )))
+  (setq org-refile-targets '((nil :maxlevel . 9)))
 
   (advice-add 'org-refile :after 'org-save-all-org-buffers)
 
@@ -331,32 +347,19 @@
 	  ("IDEA" . "LightBlue")
 	  ("KILL" . "Red")))
 
-;;  (setq org-agenda-custom-commands
-;;	'(("d" "Dashboard"
-;;	   ((agenda "" ((org-deadline-warning-days 7)))
-;;	    (todo "NEXT"
-;;		  ((org-agenda-overriding-header "Next Tasks")))
-;;	    (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
-;;
-;;	  ; Low-effort next actions
-;;	  ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
-;;	   ((org-agenda-overriding-header "Low Effort Tasks")
-;;	    (org-agenda-max-todos 20)
-;;	    (org-agenda-files org-agenda-files)))))
-
   (setq org-capture-templates
 	'(("i" "Ideas")
 	  ("ii" "Idea" entry (file+olp "~/Repos/Notes/Tasks.org" "Inbox")
 	   "* IDEA %?\n %U\n %a\n %i" :empty-lines 1)
 
-	  ("j" "Journal Entries")
-	  ("jj" "Journal" entry
-	   (file+olp+datetree "~/Repos/Notes/Journal.org")
-	   "\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
-	  :clock-in :clock-resume
+	  ("j" "Journal" entry
+           (file denote-journal-path-to-new-or-existing-entry)
+           "* %U %?\n%i\n%a"
+          :kill-buffer t
 	  :empty-lines 1)))
 
   (jclmntn/org-font-setup)
+  (org-super-agenda-mode)
   )
 
 (use-package org-bullets
@@ -368,49 +371,65 @@
   :config
   (setq org-super-agenda-groups
        '(;; Each group has an implicit boolean OR operator between its selectors.
-         (:name "Today"  ; Optionally specify section name
-                :time-grid t  ; Items that appear on the time grid
-		:date today
-		:scheduled today)
-         (:name "Work"  ; Optionally specify section name
-                ;:time-grid t  ; Items that appear on the time grid
-                :tag "work")
-         (:name "Home"  ; Optionally specify section name
-                ;:time-grid t  ; Items that appear on the time grid
-                :tag "vida")
-         (:name "Study"  ; Optionally specify section name
-                ;:time-grid t  ; Items that appear on the time grid
-                :tag ("leituras" "Estudos"))
+         (:name "Next"  ; A próxima coisa a se fazer
+                :todo "NEXT")
+         (:name "Today"  ; Coisas para fazer hoje
+                :and (
+                    :scheduled today
+                    :date today
+                    :not (:habit)))
+         (:name "Habit"  ; Tarefas diárias/semanais para manutenção da vida
+                :tag "habit"
+                :habit t)
+         (:name "Personal Sprint"  ; Tarefas relacionadas a estudos e leituras
+                :tag "sprint")
 	 )))
 
 
-(defun efs/org-mode-visual-fill ()
-  (setq visual-fill-column-width 90
+(defun jclmntn/org-mode-visual-fill ()
+  (setq visual-fill-column-width 120
         visual-fill-column-center-text t)
   (visual-fill-column-mode 1))
 
 (use-package visual-fill-column
-  :hook (org-mode . efs/org-mode-visual-fill))
+  :hook (org-mode . jclmntn/org-mode-visual-fill))
 
-(use-package org-roam
-  :config
-  (setq org-roam-directory (file-truename "~/Repos/Notes/roam"))
-  (org-roam-db-autosync-mode))
+;; (use-package org-roam
+;;   :config
+;;   (setq org-roam-directory (file-truename "~/Repos/Notes/roam"))
+;;   (setq org-roam-capture-templates
+;;         '(("d" "default" plain
+;;            "%?"
+;;            :target
+;;            (file+head
+;;             "%<%Y%m%d%H%M%S>-${slug}.org"
+;;             "#+title: ${title}\n")
+;;            :unnarrowed t)
+;;           ("n" "literature note" plain
+;;            "%?"
+;;            :target
+;;            (file+head
+;;             "%(expand-file-name (or citar-org-roam-subdir \"\") org-roam-directory)/${citar-citekey}.org"
+;;             "#+title: ${citar-citekey} (${citar-date}). ${note-title}.\n#+created: %U\n#+last_modified: %U\n\n")
+;;            :unnarrowed t)))
+;;   (org-roam-db-autosync-mode))
 
 ;;; Configurando o org-babel e o python
 (org-babel-do-load-languages
  'org-babel-load-languages
- '((python . t)))
-
+ '((python . t)
+   (eshell . t)
+   ))
 
 (setq org-confirm-babel-evaluate nil)
 
-;; LSP
+;; LSP - Python
 ;; Não esqueça de rodar interativamente o treesit-install-language-grammar 
 (use-package python
   :ensure t
   :hook ((python-ts-mode . eglot-ensure)
-	 (python-ts-mode . company-mode))
+	 (python-ts-mode . company-mode)
+	 (python-ts-mode . display-fill-column-indicator-mode))
   :mode (("\\.py\\'" . python-ts-mode)))
 
 ;; Autocomplete no buffer
@@ -419,3 +438,148 @@
   :config
   (setq company-idle-delay 0.1
 	company-minimum-prefix-length 1))
+
+;; Configurando o ambiente para programar em clojure
+(use-package cider
+  :bind (("C-c u" . cider-user-ns)
+	 ("C-M-r" . cider-refresh))
+  :hook ((cider-repl-mode . company-mode)
+	 (clojure-mode . company-mode)
+	 (clojure-mode . display-fill-column-indicator-mode))
+;;  :mode (("\\.clj\\'" . cider-mode))
+  :config
+  (setq cider-show-error-buffer t
+	cider-auto-select-error-buffer t
+	cider-repl-history-file "~/.emacs.d/cider-history"
+	cider-repl-pop-to-buffer-on-connect t
+	cider-repl-wrap-history t))
+
+;; Ediff configuration
+(setq ediff-split-window-function 'split-window-horizontally)
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+
+;; Saves desktop/session
+(desktop-save-mode 1)
+
+;; Fuck TABS
+(setq-default indent-tabs-mode nil)
+
+;; Yasnippets
+(use-package yasnippet
+  :config
+  (setq yas-snippet-dirs '("~/Repos/Notes/Snippets/"))
+  (yas-global-mode 1))
+
+;; Manage backups
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup/")))
+
+;; Spellchecking
+;; (use-package jinx
+;;   :hook (org-mode . jinx-mode)
+;;   :bind (("M-$" . jinx-correct)
+;;        ("C-M-$" . jinx-languages)))
+
+;; Testes com org-noter
+
+(use-package pdf-tools
+  :config
+  (pdf-tools-install))
+(use-package org-noter)
+
+
+(use-package denote
+  :defer t
+  :custom
+  (denote-directory "~/documents/notes"))
+
+;; Testes com ambientes virtuais no python
+(use-package pet 
+  :config (add-hook 'python-base-mode-hook 'pet-mode -10))
+
+;; Para otimizar busca de notas nas duas bases de código
+(use-package consult-notes
+  :commands (consult-notes
+             consult-notes-search-in-all-notes
+             ;; if using org-roam 
+             consult-notes-org-roam-find-node
+             consult-notes-org-roam-find-node-relation)
+  :config
+  (setq consult-notes-file-dir-sources '(("Roam"  ?r  "~/Repos/Notes/roam/")
+                                         ("Denote" ?d "~/Repos/Notes/denote-notes/"))) ;; Set notes dir(s), see below
+  (consult-notes-org-headings-mode)
+  (when (locate-library "denote")
+    (consult-notes-denote-mode)))
+
+;; Testes com Denote
+(use-package denote
+  :ensure t
+  :hook (dired-mode . denote-dired-mode)
+  :config
+  (setq denote-directory (expand-file-name "~/Repos/Notes/denote-notes"))
+  ;; Denote buffers automatically renamed to have prefix + title
+  (denote-rename-buffer-mode 1))
+
+;; Adicionando MSYS2 ao path
+(add-to-list 'exec-path "C:/msys64/usr/bin/")
+(setenv "PATH" (mapconcat #'identity exec-path path-separator))
+
+;; Configurando o Denote Journal
+;; Não se esqueça de baixar uma cópia local do denote de lá do github
+(add-to-list 'load-path "~/.emacs.d/manual-packages/denote-journal")
+
+(use-package denote-journal
+  :ensure t
+  ;; Bind those to some key for your convenience.
+  :hook (calendar-mode . denote-journal-calendar-mode)
+  :config
+  ;; Use the "journal" subdirectory of the `denote-directory'.  Set this
+  ;; to nil to use the `denote-directory' instead.
+  (setq denote-journal-directory
+        (expand-file-name "journal" denote-directory))
+  ;; Default keyword for new journal entries. It can also be a list of
+  ;; strings.
+  (setq denote-journal-keyword "journal")
+  ;; Read the doc string of `denote-journal-title-format'.
+  (setq denote-journal-title-format 'day-date-month-year))
+
+;; Testes com citar
+(use-package citar
+  :ensure t
+  :defer t
+  :custom
+  ;; set bibliography's location
+  (citar-bibliography '("~/Repos/Notes/bibs/lista_de_leitura.bib" "~/Repos/Notes/bibs/mba_literatura.bib"))
+  ;; Allow multiple notes per bibliographic entry
+  (citar-open-always-create-notes nil)
+  :bind ("C-c w c" . citar-create-note))
+
+(use-package citar-denote
+:ensure t
+:demand t ;; Ensure minor mode loads
+:after (:any citar denote)
+:custom
+;; Package defaults
+(citar-denote-file-type 'org)
+(citar-denote-keyword "bib")
+(citar-denote-subdir "~/Repos/Notes/denote-notes/bib/")
+(citar-denote-template nil)
+(citar-denote-title-format "author-year-title")
+(citar-denote-title-format-andstr "and")
+(citar-denote-title-format-authors 1)
+:init
+(citar-denote-mode))
+;; Bind all available commands
+;; :bind (("C-c w d" . citar-denote-dwim)
+;;        ("C-c w e" . citar-denote-open-reference-entry)
+;;        ("C-c w a" . citar-denote-add-citekey)
+;;        ("C-c w k" . citar-denote-remove-citekey)
+;;        ("C-c w r" . citar-denote-find-reference)
+;;        ("C-c w l" . citar-denote-link-reference)
+;;        ("C-c w f" . citar-denote-find-citation)
+;;        ("C-c w x" . citar-denote-nocite)
+;;        ("C-c w y" . citar-denote-cite-nocite)
+;;        ("C-c w z" . citar-denote-nobib)))
+
+;; Set search as ripgrep for many programs
+;; Funciona com o citar-denote!!!
+(setq xref-search-program 'ripgrep)
